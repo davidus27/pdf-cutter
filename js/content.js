a83971162f0736b554e0c482ce75d1c9f5f36fd1
davidus27 <davidisbest27@gmail.com>
2021-01-24 18:23:04 +0100

    add content communication

--- js/content.js
+++ js/content.js
@@ -1,5 +1,23 @@
+const message = {title: window.document.title};
+
+
+const isPDF = () => {
+    // if window contains one of this it is probably a pdf file
+    return window.MimeTypes || window.PdfNavigator || /\.pdf/i.exec(window.location.href);
+}
+
+// if url is ending with .pdf
+message['pdf'] = isPDF();
+console.log("whaayy? ", message);
+
+
+chrome.runtime.sendMessage(message, (response) => {
+    //console.log(response);
+});
+
+
 const gotMessage = (message, sender, sendResponse) => {
-    alert(message.txt);
+
 }
 
 chrome.runtime.onMessage.addListener(gotMessage);
