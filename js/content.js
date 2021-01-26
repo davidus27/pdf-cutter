906abec0bcc4c3ae6ffc576d635176773c8eaf8a
davidus27 <davidus27@gmail.com>
2021-01-26 17:26:15 +0100

    added simple communication listener in content file

--- js/content.js
+++ js/content.js
@@ -1,23 +1,11 @@
-const message = {title: window.document.title};
-
-
 const isPDF = () => {
     // if window contains one of this it is probably a pdf file
-    return window.MimeTypes || window.PdfNavigator || /\.pdf/i.exec(window.location.href);
+    return Boolean(window.MimeTypes || window.PdfNavigator || window.location.href.match(/\.pdf/i));
 }
 
-// if url is ending with .pdf
-message['pdf'] = isPDF();
-console.log("whaayy? ", message);
 
-
-chrome.runtime.sendMessage(message, (response) => {
-    //console.log(response);
-});
-
-
-const gotMessage = (message, sender, sendResponse) => {
-
-}
-
-chrome.runtime.onMessage.addListener(gotMessage);
+chrome.runtime.onConnect.addListener(function(port) {
+    port.onMessage.addListener((message) => {
+      port.postMessage({title:window.location.href, pdf:isPDF()});
+    });
+  });
