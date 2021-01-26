6bed4d67d44ec387aa2fa4342dc3c41b19dd0db8
davidus27 <davidus27@gmail.com>
2021-01-26 22:35:35 +0100

    returning communication after button clicked

--- js/content.js
+++ js/content.js
@@ -3,9 +3,19 @@ const isPDF = () => {
     return Boolean(window.MimeTypes || window.PdfNavigator || window.location.href.match(/\.pdf/i));
 }
 
+const processPDF = () => {
+    // work with the pdf content
+    
+}
 
-chrome.runtime.onConnect.addListener(function(port) {
+chrome.runtime.onConnect.addListener((port) => {
     port.onMessage.addListener((message) => {
-      port.postMessage({title:window.location.href, pdf:isPDF()});
+        if(message["state"] == "start") {
+            processPDF();
+            port.postMessage({"state":"done"});
+        }
+        else {
+            port.postMessage({"title":window.location.href, "pdf":isPDF()});
+        }
     });
-  });
+});
