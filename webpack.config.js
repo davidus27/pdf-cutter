72507df9d0e513033844cb207110aded399307f9
davidus27 <davidus27@gmail.com>
2021-01-31 00:11:07 +0100

    added more specific config for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,13 @@
+const path = require('path');
+ 
 module.exports = {
-    entry: "./src/content/content.js",
-    mode: "production",
-}
+    entry: './src/content/content.js',
+    mode: 'development',
+    entry: {
+        index: './src/content/content.js'
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
