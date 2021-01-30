51a4722b52f45bd182482265cfcd61f1ac9d28f3
davidus27 <davidus27@gmail.com>
2021-01-31 00:09:41 +0100

    added base of highlight detection

--- src/content/content.js
+++ src/content/content.js
@@ -1,14 +1,21 @@
-import * as PDFJS from 'pdfjs-dist';
-import * as PDF from './content_functions.js';
+import * as contentFunctions from './content_functions.js'
+
+import * as pdfjs from 'pdfjs-dist'
+import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry'; 
+pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker; 
+
+//pdfjs.GlobalWorkerOptions.workerSrc = require("pdfjs-dist/build/pdf.worker.entry.js");
+
 
 chrome.runtime.onConnect.addListener((port) => {
     port.onMessage.addListener((message) => {
-        if(message["state"] == "start") {
-            PDF.processPDF();
+        if(message["state"] == "start" && contentFunctions.isPDF()) {
+            console.log(window.location.href);
+            contentFunctions.processPDF(pdfjs.getDocument(window.location.href).promise);
             port.postMessage({"state":"done"});
         }
         else {
-            port.postMessage({"title":window.location.href, "pdf":PDF.isPDF()});
+            port.postMessage({"title":window.location.href, "pdf":contentFunctions.isPDF()});
         }
     });
 });
--- src/content/content_functions.js
+++ src/content/content_functions.js
@@ -1,11 +1,65 @@
+import { console } from 'globalthis/implementation';
+
+
 const isPDF = () => {
     // if window contains one of this it is probably a pdf file
     return Boolean(window.MimeTypes || window.PdfNavigator || window.location.href.match(/\.pdf/i));
 }
 
-const processPDF = () => {
-    // work with the pdf content
-    
+const processDocument = async (pdf, callbackProcessPage = processPage) => {
+    const pageAmount = pdf._pdfInfo.numPages;
+    let currentPage;
+    let highlightedPages = [];
+
+    for(let currentPageIndex = 1; currentPageIndex <= pageAmount; currentPageIndex++) {
+        currentPage = pdf.getPage(currentPageIndex);
+        await currentPage
+            .then((page) => {
+                const hValue = callbackProcessPage(page);
+                console.log("Return value:", hValue);
+                if(hValue) {
+                    highlightedPages.push(currentPage);
+                }
+            })
+            .catch((err) => console.log(err));
+    }
+    return highlightedPages;
 }
+
+const processPage = async (page) => { 
+    // returns boolean if currentPage contains highlighted content
+    await page.getAnnotations()
+        .then((pageAnnotations) => {
+            // finds if at least one anotation on page is the Highlight
+            //console.log("Page: ", page._pageIndex,pageAnnotations.filter(annotation => annotation.subtype === "Highlight"));
+            return pageAnnotations.filter(annotation => annotation.subtype === "Highlight");
+            //console.log("Page num.", currentPageIndex, pageAnnotation.subtype, pageAnnotation);
+            //if(pageAnnotation.subtype == "Highlight") {
+        })
+        .catch((err) => {
+            console.log("There is a problem with annotation:", err)
+            return false;
+        });
+}
+
+
+const onFinishDocument = () => {
+    // what you want to do after document is processed
+}
+
+const processPDF = async (documentPromise, callbackProcessDocument = processDocument, callbackAllDone = onFinishDocument) => {
+    // work with the pdf content
+    let highlightedPages;
+    documentPromise
+        .then(async (pdf) => {
+            highlightedPages = await callbackProcessDocument(pdf);
+            console.log(highlightedPages);
+            })
+        .catch((err) => {
+            console.log("error: ", err);
+        });
+    callbackAllDone();
+}
+
 
 export {isPDF, processPDF};
