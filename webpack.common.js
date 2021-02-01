47849381e8fd650c7d3f2ee90489a3d9a3cf0ad2
davidus27 <davidus27@gmail.com>
2021-02-01 22:18:10 +0100

    removed extra plugin in webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,9 @@
 const path = require('path');
- const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
  module.exports = {
    entry: {
      app: './src/content/content.js',
    },
-   plugins: [
-     new CleanWebpackPlugin()
-   ],
    output: {
      filename: 'main.js',
      path: path.resolve(__dirname, 'dist'),
