0431beb1cba1d41b1fd9975ef7a4c619c2b53e88
davidus27 <davidus27@gmail.com>
2021-02-01 19:44:09 +0100

    changed order of loading listeners for better interactivity with the popup

--- src/content/content.js
+++ src/content/content.js
@@ -1,30 +1,24 @@
 import * as contentFunctions from './content_functions.js'
 
-const addChromeListener = (callbackMessageSent) => {
-    chrome.runtime.onConnect.addListener((port) => {
-        port.onMessage.addListener((message) => {
-            if(message["state"] == "start" && contentFunctions.isPDF()) {
-                callbackMessageSent().then((messageSent) => {
-                    const message = messageSent ? {"state":"done"} : {"state":"noPageFound"};
-                    port.postMessage(message);
-                });    
-            }
-            else {
-                const title = contentFunctions.processTitle(window.location.href);
-                port.postMessage({"title":title, "pdf":contentFunctions.isPDF()});
-            }
-        });
-    })
-}
-
 const creator = new contentFunctions.NewDocumentCreator(window.location.href);
-creator.initialize()
-    .then(() => {
-        addChromeListener(() => {
-            creator.findPages().removeExtraPages();
-            return creator.createNewPDF();
-        });
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+const creatorInitialization = creator.initialize();
+
+chrome.runtime.onConnect.addListener((port) => {
+    port.onMessage.addListener((message) => {
+        if(message["state"] == "init") {
+            creatorInitialization.then(() => {
+                const title = contentFunctions.processTitle(window.location.href);
+                port.postMessage({"state":"ready", "title":title, "pdf":contentFunctions.isPDF()});
+            })
+        }
+        else if(message["state"] == "start" && contentFunctions.isPDF()) {
+            creatorInitialization.then(() => {
+                creator.findPages().removeExtraPages();
+                creator.createNewPDF().then((pdfProcessed) => {
+                    const message = pdfProcessed ? {"state":"done"} : {"state":"noPageFound"};
+                    port.postMessage(message);
+                })
+            });    
+        }
+    });
+})
