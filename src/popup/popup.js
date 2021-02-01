92c010ab4694ae05295336f241beedcce2321e80
davidus27 <davidus27@gmail.com>
2021-02-01 18:54:02 +0100

    added new loading screen on loadPDFDoc and added error screen if there is no highlight on page

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,5 +1,7 @@
 
 const showScreen = (message) => {
+    document.querySelector("#loading-screen").style.display = "none";
+
     const pdfFoundElem = document.querySelector("#pdf-found-div");
     const pdfNotFoundElem = document.querySelector("#pdf-not-found-div");
     if(message["pdf"]) {
@@ -13,6 +15,7 @@ const showScreen = (message) => {
 }
 
 const startLoading = () => {
+    document.querySelector("#loading-txt").textContent = "Loading...";
     document.querySelector("#loading-screen").style.display = "inline"; // show loading screen
     document.querySelector("#cut-btn").classList.add("disabled"); // disable button
 }
@@ -22,18 +25,27 @@ const endLoading = () => {
     document.querySelector("#cut-btn").classList.remove("disabled"); // disable button
 }
 
+const showPageNotFound = (title) => {
+    document.querySelector("#pdf-found-div h1").textContent = `No occurance found in PDF ${title}`;
+}
 
 chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
     const port = chrome.tabs.connect(tabs[0].id, {name: "connection"});
+    let pageTitle;
     port.postMessage({"state":"init"});
     port.onMessage.addListener((message) => {
-        if(message["pdf"])
+        if(message["pdf"]) {
+            pageTitle = message["title"];
             showScreen(message);
+        }
     });
 
     document.querySelector("#cut-btn").addEventListener("click", () => {
         port.postMessage({"state":"start"})
         startLoading();
-        port.onMessage.addListener((message) => {if(message["state"] == "done") endLoading();});
+        port.onMessage.addListener((message) => {
+            endLoading();
+            if(message["state"] == "noPageFound") showPageNotFound(pageTitle);
+        });
     });
 });
